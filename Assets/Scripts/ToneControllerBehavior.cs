7e0dfced87e746a1d541de9fb8866b2089bbabae
Juan Pablo Artola <juanpabloartola@gmail.com>
2020-05-22 17:35:27 -0300

    added multiple sounds

--- Assets/Scripts/ToneControllerBehavior.cs
+++ Assets/Scripts/ToneControllerBehavior.cs
@@ -12,6 +12,7 @@ namespace Toneitor
 
         [SerializeField] private string startTone = "C";
         [SerializeField] private int startOctave = 2;
+        [SerializeField] private int longScale = 8;
 
         [SerializeField] private float[] rithm = new float[] { 1, 0.5f, 0.5f, 1, 0.5f, 0.5f };
 
@@ -21,12 +22,12 @@ namespace Toneitor
 
         private IEnumerator Start() {
             int index = 0;
-            currentScale = tc.GetScale(tc.GetOctaveTone(startTone, startOctave), 12, ToneController.MajorScale);
+            currentScale = tc.GetScale(tc.GetOctaveTone(startTone, startOctave), longScale, ToneController.MajorScale);
             OctaveTone octaveTone;
             int rithmLenght = rithm.Length;
             float time = 0;
             while (true) {
-                octaveTone = currentScale[UnityEngine.Random.Range(0, 9)];
+                octaveTone = currentScale[UnityEngine.Random.Range(0, longScale)];
                 proceduralAudio.frequency = octaveTone.Frequency;
                 //Debug.Log(octaveTone.Name);
                 time = rithm[index] * 60 / tempo;
