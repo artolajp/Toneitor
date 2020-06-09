4ab5f32e484271ae0557d842be5aebd3c44c4ca0
Juan Pablo Artola <juanpabloartola@gmail.com>
2020-06-09 17:42:06 -0300

    update unity version

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Toneitor {
+    public class SoundController : MonoBehaviour
+    {
+        [SerializeField] private ToneControllerBehaviour toneControllerBehaviourPrefab = null;
+
+        [SerializeField] private Transform toneControllersContainers = null;
+
+        //return index of controller
+        public int PlayTone(OctaveTone tone, float seg ) {
+
+            return 0;
+        }
+
+    }
+}
